1436e373bbc3e7643ecffbfe2ea2925ad8155a2e
Joway Wang <joway.w@gmail.com>
2017-08-24 02:00:11 +0800

    add admin

--- api/todo/admin.py
+++ api/todo/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from todo.models import TodoItem, TodoList
 
-admin.site.register(TodoItem)
-admin.site.register(TodoList)
+
+@admin.register(TodoItem)
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'done', 'created_at')
+
+
+@admin.register(TodoList)
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'items', 'created_at')
