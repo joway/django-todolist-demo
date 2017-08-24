f871db10d82cda2888a34ccf977cc4e1cdfd92d5
Joway Wang <joway.w@gmail.com>
2017-08-24 10:40:35 +0800

    change auth policy

--- api/config/settings.py
+++ api/config/settings.py
@@ -70,7 +70,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'user.auth.CookieAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
